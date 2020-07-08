fe86de434d5d60983c81792316229c7d10a83df5
Donald <dnldcode@gmail.com>
2020-07-08 21:36:44 +0200

    Web routes changed to plural(/type/id -> /types/id)

--- routes/web.php
+++ routes/web.php
@@ -6,9 +6,9 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', 'HomeController@index')->name('home');
 Route::get('/api', 'Docs\ApiController@index')->name('api');
 Route::get('/methods', 'Docs\ApiController@methods')->name('methods');
-Route::get('/method/{method}', 'Docs\ApiController@method')->name('method');
+Route::get('/methods/{method}', 'Docs\ApiController@method')->name('method');
 Route::get('/types', 'Docs\ApiController@types')->name('types');
-Route::get('/type/{type}', 'Docs\ApiController@type')->name('type');
+Route::get('/types/{type}', 'Docs\ApiController@type')->name('type');
 
 /* Admin Panel */
 Route::middleware('auth')->group(function () {

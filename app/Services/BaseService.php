4c830b09b625499083e6f24d8f7017afd4344ab5
Donald <dnldcode@gmail.com>
2020-07-08 21:17:42 +0200

    BaseService created

---
+++ app/Services/BaseService.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Services;
+
+abstract class BaseService
+{
+    public function all()
+    {
+        return $this->model->all();
+    }
+
+    public function create(array $input)
+    {
+        return $this->model->create($input);
+    }
+
+    public function find($id)
+    {
+        return $this->model->find($id);
+    }
+
+    public function destroy($id)
+    {
+        return $this->find($id)->delete();
+    }
+
+    public function update($id, array $input)
+    {
+        return $this->find($id)->update($input);
+    }
+}

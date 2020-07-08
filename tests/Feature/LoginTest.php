91174f95ff42ba1f9fbcd2fb2ef39ee9e96e7bc9
Donald <dnldcode@gmail.com>
2020-07-08 23:50:34 +0200

    Tests created for Login/Logout

---
+++ tests/Feature/LoginTest.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\User;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+class LoginTest extends TestCase
+{
+    use RefreshDatabase;
+
+    /**
+     * A basic feature test example.
+     *
+     * @return void
+     */
+    public function test_user_can_view_a_login_form()
+    {
+        $response = $this->get('/login');
+
+        $response->assertSuccessful();
+        $response->assertViewIs('auth.login');
+    }
+
+    public function test_user_cannot_view_a_login_form_when_authenticated()
+    {
+        $user = factory(User::class)->make();
+
+        $response = $this->actingAs($user)->get('/login');
+
+        $response->assertRedirect('/panel');
+    }
+
+    public function test_user_can_login_with_correct_credentials()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $response->assertRedirect('/panel');
+        $this->assertAuthenticatedAs($user);
+    }
+
+    public function test_user_cannot_login_with_incorrect_password()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt('i-love-laravel'),
+        ]);
+
+        $response = $this->from('/login')->post('/login', [
+            'email' => $user->email,
+            'password' => 'invalid-password',
+        ]);
+
+        $response->assertRedirect('/login');
+        $response->assertSessionHasErrors('email');
+        $this->assertTrue(session()->hasOldInput('email'));
+        $this->assertFalse(session()->hasOldInput('password'));
+        $this->assertGuest();
+    }
+
+    public function test_user_can_logout()
+    {
+        $user = factory(User::class)->create([
+            'password' => bcrypt($password = 'i-love-laravel'),
+        ]);
+
+        $response = $this->post('/login', [
+            'email' => $user->email,
+            'password' => $password,
+        ]);
+
+        $this->post('logout');
+        $this->assertGuest();
+    }
+}

ee0a79fcf1bc56b0403fcb60e2395135006ae85a
Donald <dnldcode@gmail.com>
2020-07-09 21:07:22 +0200

    MethodRequest created

---
+++ app/Http/Requests/MethodRequest.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Requests;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class MethodRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            'tag_id' => 'required|exists:method_tags,id',
+            'name' => 'required|min:2',
+            'type' => 'required|exists:methods,type',
+            'description' => 'required|min:5|max:300',
+            'return_type_id' => 'required|exists:types,id',
+        ];
+    }
+}

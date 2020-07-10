07988b9d9f06ed697d2f86f308b3cf47d24d48c4
Donald <dnldcode@gmail.com>
2020-07-10 19:01:36 +0200

    Migrations/Controllers/Models created for error and error_categories tables

---
+++ app/Http/Controllers/ErrorCategoryController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\ErrorCategory;
+use Illuminate\Http\Request;
+
+class ErrorCategoryController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\ErrorCategory  $errorCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function show(ErrorCategory $errorCategory)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\ErrorCategory  $errorCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(ErrorCategory $errorCategory)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\ErrorCategory  $errorCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, ErrorCategory $errorCategory)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\ErrorCategory  $errorCategory
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(ErrorCategory $errorCategory)
+    {
+        //
+    }
+}

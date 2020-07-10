336b0a8a575a3f6070e76e366dd3c5d53bb37fce
Donald <dnldcode@gmail.com>
2020-07-10 21:07:37 +0200

    Admin layout template updated

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -20,6 +20,17 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css"
           integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 
+    <!-- Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"
+            integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj"
+            crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"
+            integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo"
+            crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/js/bootstrap.min.js"
+            integrity="sha384-OgVRvuATP1z7JjHLkuOU7Xw704+h835Lr+6QL9UvYjZE3Ipu6Tp75j7Bh/kR0JKI"
+            crossorigin="anonymous"></script>
+
     <!-- Styles -->
     <link href="{{ asset('css/app.css') }}" rel="stylesheet">
     <link href="{{ asset('css/core.css') }}" rel="stylesheet">
@@ -41,15 +52,32 @@
                     <li class="nav-item">
                         <a class="nav-link" href="{{ route('admin.types') }}">Types</a>
                     </li>
-                    <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.methods') }}">Methods</a>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                           aria-haspopup="true" aria-expanded="false">Methods</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="{{ route('admin.methods') }}">Methods</a>
+                            <a class="dropdown-item" href="{{ route('admin.method-tags') }}">Tags</a>
+                        </div>
+                    </li>
+
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" data-toggle="dropdown" href="#" role="button"
+                           aria-haspopup="true" aria-expanded="false">Errors</a>
+                        <div class="dropdown-menu">
+                            <a class="dropdown-item" href="#">Erros</a>
+                            <a class="dropdown-item" href="#">Categories</a>
+                        </div>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="{{ route('admin.method-tags') }}">Method Tags</a>
+                        <a class="nav-link" href="#">Users</a>
                     </li>
+
                     <li class="nav-item">
-                        <a class="nav-link btn-link text-primary underline" href="{{ route('home') }}"><u>Go on main page</u></a>
+                        <a class="nav-link btn-link text-primary underline" href="{{ route('home') }}"><u>Go on main
+                                page</u></a>
                     </li>
                 </ul>
                 <a class="log-out-btn" href="#"

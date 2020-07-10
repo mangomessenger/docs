f6c488869900c3a67220c3ee480f7f22ca81bc56
Donald <dnldcode@gmail.com>
2020-07-10 19:04:53 +0200

    Error and ErrorCategory Controllers moved to Admin folder

--- app/Http/Controllers/Admin/ErrorCategoryController.php
+++ app/Http/Controllers/Admin/ErrorCategoryController.php
@@ -1,8 +1,9 @@
 <?php
 
-namespace App\Http\Controllers;
+namespace App\Http\Controllers\Admin;
 
 use App\ErrorCategory;
+use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 
 class ErrorCategoryController extends Controller

c96f816472a8b79e71d1fe89483a6a9baae2a83b
Donald <dnldcode@gmail.com>
2020-07-10 23:14:13 +0200

    Many To Many intermidiate table migration created for methods and errors & Previous migrations naming fix

--- database/migrations/2020_07_10_211057_create_error_method_table.php
+++ database/migrations/2020_07_10_211057_create_error_method_table.php
@@ -4,7 +4,7 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-class CreateMethodErrorTable extends Migration
+class CreateErrorMethodTable extends Migration
 {
     /**
      * Run the migrations.
@@ -13,11 +13,10 @@ class CreateMethodErrorTable extends Migration
      */
     public function up()
     {
-        Schema::create('method_error', function (Blueprint $table) {
+        Schema::create('error_method', function (Blueprint $table) {
             $table->id();
             $table->foreignId('method_id')->constrained('methods');
             $table->foreignId('error_id')->constrained('errors');
-            $table->timestamps();
         });
     }
 
@@ -28,6 +27,6 @@ class CreateMethodErrorTable extends Migration
      */
     public function down()
     {
-        Schema::dropIfExists('method_error');
+        Schema::dropIfExists('error_method');
     }
 }

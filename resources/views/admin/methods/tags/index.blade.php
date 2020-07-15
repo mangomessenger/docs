ec3e26bd9f15f8fdda148e77c86990294c1fbbe6
Donald <dnldcode@gmail.com>
2020-07-15 21:21:59 +0200

    Button display fix in index pages of tags/categories/errors etc.

--- resources/views/admin/methods/tags/index.blade.php
+++ resources/views/admin/methods/tags/index.blade.php
@@ -2,12 +2,15 @@
 
 @section('content')
     <div class="container">
-        <div class="row">
-            <h2>Method Tags</h2>
-        </div>
 
-        <div class="row">
-            <a class="btn btn-primary" href="{{ route('methods.tags.create') }}">Create new</a>
+        <div class="row pt-3">
+            <div class="col-lg-9 col-md-8 col-7">
+                <h4 class="font-weight-bold">Method Tags</h4>
+            </div>
+
+            <div class="col-lg-3 col-md-4 col-5">
+                <a class="btn btn-primary w-100" href="{{ route('methods.tags.create') }}">Create new</a>
+            </div>
         </div>
 
         <div class="row py-3 table-responsive">

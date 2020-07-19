745c5631ffd2bddd5e35e8dde18e7d73e704c311
Donald <dnldcode@gmail.com>
2020-07-19 18:09:41 +0200

    Pagination added to BaseService & PHPDoc Update

--- app/Services/BaseService.php
+++ app/Services/BaseService.php
@@ -4,28 +4,57 @@ namespace App\Services;
 
 abstract class BaseService
 {
+    /**
+     * @return mixed
+     */
     public function all()
     {
         return $this->model->all();
     }
 
+    /**
+     * @param array $input
+     * @return mixed
+     */
     public function create(array $input)
     {
         return $this->model->create($input);
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function find($id)
     {
         return $this->model->findOrFail($id);
     }
 
+    /**
+     * @param $id
+     * @return mixed
+     */
     public function delete($id)
     {
         return $this->find($id)->delete();
     }
 
+    /**
+     * @param $id
+     * @param array $input
+     * @return mixed
+     */
     public function update($id, array $input)
     {
         return $this->find($id)->update($input);
+    }
+
+    /**
+     * @param int $count
+     * @return mixed
+     */
+    public function paginate(int $count = 20)
+    {
+        return $this->model->paginate($count);
     }
 }
